9e1bc8bd8645144b202543dd4aa9ae32a28eb275
Fredrik Norrman <fn222ep@student.lnu.se>
2020-03-13 00:11:34 +0100

    app.js skeleton

---
+++ app.js
@@ -0,0 +1,48 @@
+/**
+ * The starting point of the application.
+ *
+ * @author Fredrik Norrman
+ * @version 1.0.0
+ */
+
+'use strict'
+
+const express = require('express')
+const hbs = require('express-hbs')
+const { join } = require('path')
+const logger = require('morgan')
+const app = express()
+
+// view engine setup
+app.engine('hbs', hbs.express4({
+  defaultLayout: join(__dirname, 'views', 'layouts', 'default'),
+  partialsDir: join(__dirname, 'views', 'partials')
+}))
+app.set('view engine', 'hbs')
+app.set('views', join(__dirname, 'views'))
+
+// additional middleware
+app.use(logger('dev'))
+app.use(express.urlencoded({ extended: false }))
+app.use(express.static(join(__dirname, 'public')))
+
+// routes
+app.use('/', require('./routes/homeRouter'))
+
+// Error handler.
+app.use((err, req, res, next) => {
+  // 404 Not Found.
+  if (err.statusCode === 404) {
+    return res.status(404).sendFile(join(__dirname, 'views', 'errors', '404.html'))
+  }
+  // 500 Internal Server Error (in production, all other errors send this response).
+  if (req.app.get('env') !== 'development') {
+    return res.status(500).sendFile(join(__dirname, 'views', 'errors', '500.html'))
+  }
+
+  // Render the error page.
+  res.status(err.statusCode || 500).render('errors/error', { err })
+})
+
+// listen to provided port
+app.listen(3000, () => console.log('Server running at http://localhost:3000'))
