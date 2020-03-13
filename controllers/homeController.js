86ba5546f58be4b93dba650cceaa3a86d84d1caf
Fredrik Norrman <fn222ep@student.lnu.se>
2020-03-13 06:48:25 +0100

    try catch on api call

--- controllers/homeController.js
+++ controllers/homeController.js
@@ -12,31 +12,38 @@ const fetch = require('node-fetch')
 const moment = require('moment')
 require('dotenv').config()
 
+// GitLab api URL
 const url = `https://gitlab.lnu.se/api/v4/projects/2651/issues?private_token=${process.env.ACCESS_TOKEN}`
 
 /**
- * Renders the index page.
+ * Fetching all known issues and rendering the index page.
  *
  * @param {object} req - Express request object.
  * @param {object} res - Express response object.
  */
 
 homeController.index = async (req, res) => {
-  const request = await fetch(url)
-  const json = await request.json()
-  const viewData = {
-    issues: json.map(issue => ({
-      author: issue.author.name,
-      username: issue.author.username,
-      title: issue.title,
-      description: issue.description,
-      url: issue.web_url,
-      comments: issue.user_notes_count,
-      createdAt: moment(issue.created_at).format('YYYY-MM-DD HH:mm'),
-      updatedAt: moment(issue.updated_at).format('YYYY-MM-DD HH:mm')
-    }))
+  try {
+    const request = await fetch(url)
+    const json = await request.json()
+    const viewData = {
+      issues: json.map(issue => ({
+        author: issue.author.name,
+        username: issue.author.username,
+        title: issue.title,
+        description: issue.description,
+        url: issue.web_url,
+        comments: issue.user_notes_count,
+        createdAt: moment(issue.created_at).format('YYYY-MM-DD HH:mm'),
+        updatedAt: moment(issue.updated_at).format('YYYY-MM-DD HH:mm')
+      }))
+    }
+    res.render('home/index', { viewData })
+  } catch (error) {
+    console.error(error)
+    res.render('home/index')
   }
-  res.render('home/index', { viewData })
 }
 
+// Exporting module
 module.exports = homeController
