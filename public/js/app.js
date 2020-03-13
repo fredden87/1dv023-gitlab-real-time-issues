7431d0b93b0ebadc600e74468752e08a02a8d08b
Fredrik Norrman <fn222ep@student.lnu.se>
2020-03-13 22:52:07 +0100

    Implementing client code

--- public/js/app.js
+++ public/js/app.js
@@ -1,15 +1,47 @@
 const io = window.io('/')
-// const issue = document.querySelector('#issue-template').content.cloneNode(true)
-// const issueNodeList = issue.querySelectorAll('*')
-// console.log(issue)
 
 // Listening to event 'webhook-event', server will send data here from webhook.
 io.on('webhook-event', function (event) {
   if (event.eventType === 'issue') {
-    console.log(event)
+    createIssueCard(event)
   } else if (event.eventType === 'note') {
-    console.log(event)
+    // console.log(event)
   }
 })
 
 // document.querySelector('#issues').prepend(issue)
+/**
+ * @param issueData
+ */
+function createIssueCard (issueData) {
+  const issueDataArr = Object.values(issueData)
+  const issueCard = document.querySelector('#issue-template').content
+    .cloneNode(true).firstElementChild
+
+  issueCard.forEach(element => {
+    console.log(element)
+  })
+
+  document.querySelector('#issues').prepend(issueCard)
+}
+
+/**
+ * @param noteData
+ */
+function createNotificationCard (noteData) {
+
+}
+
+/**
+ * @param elements
+ * @param elem
+ */
+function getChildAndSiblings (elements) {
+  for (const element of elements) {
+    if (element.nextChild) {
+      console.log(element.nextChild)
+    } else if (element.nextSibling) {
+      console.log(element.nextSibling.previousElementSibling)
+    }
+  }
+}
