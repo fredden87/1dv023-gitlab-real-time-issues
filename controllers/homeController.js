762de73623f475e87412c9eec0398417f1a2fd27
Fredrik Norrman <fn222ep@student.lnu.se>
2020-03-13 06:41:06 +0100

    Now rendering issues from API

--- controllers/homeController.js
+++ controllers/homeController.js
@@ -9,6 +9,7 @@
 
 const homeController = {}
 const fetch = require('node-fetch')
+const moment = require('moment')
 require('dotenv').config()
 
 const url = `https://gitlab.lnu.se/api/v4/projects/2651/issues?private_token=${process.env.ACCESS_TOKEN}`
@@ -23,12 +24,19 @@ const url = `https://gitlab.lnu.se/api/v4/projects/2651/issues?private_token=${p
 homeController.index = async (req, res) => {
   const request = await fetch(url)
   const json = await request.json()
-  const issues = {
-    title: json[0].title,
-    description: json[0].description
+  const viewData = {
+    issues: json.map(issue => ({
+      author: issue.author.name,
+      username: issue.author.username,
+      title: issue.title,
+      description: issue.description,
+      url: issue.web_url,
+      comments: issue.user_notes_count,
+      createdAt: moment(issue.created_at).format('YYYY-MM-DD HH:mm'),
+      updatedAt: moment(issue.updated_at).format('YYYY-MM-DD HH:mm')
+    }))
   }
-  console.log(issues)
-  res.render('home/index')
+  res.render('home/index', { viewData })
 }
 
 module.exports = homeController
