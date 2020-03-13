1c84690dd117dd0f0b41dcf3cfad95659b971d77
Fredrik Norrman <fn222ep@student.lnu.se>
2020-03-13 05:57:09 +0100

    install dotenv and started to make api calls

--- controllers/homeController.js
+++ controllers/homeController.js
@@ -9,15 +9,25 @@
 
 const homeController = {}
 const fetch = require('node-fetch')
+require('dotenv').config()
+
+const url = `https://gitlab.lnu.se/api/v4/projects/2651/issues?private_token=${process.env.ACCESS_TOKEN}`
 
 /**
- * Renders the start page.
+ * Renders the index page.
  *
  * @param {object} req - Express request object.
  * @param {object} res - Express response object.
  */
 
 homeController.index = async (req, res) => {
+  const request = await fetch(url)
+  const json = await request.json()
+  const issues = {
+    title: json[0].title,
+    description: json[0].description
+  }
+  console.log(issues)
   res.render('home/index')
 }
 
